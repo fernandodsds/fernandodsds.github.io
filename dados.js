6213e3c736bdb1cda200ba9603cff1c4ce2a2775
fernandodsds <31160508+fernandodsds@users.noreply.github.com>
2018-01-15 15:44:58 -0200

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,14 +1,14 @@
 var dados = [
   {"linha":"Meu nome é Fernando dos Santos da Silva"},
-  {"linha":"Sou natural de Campinas-SP, tenho 18 anos"},
-  {"linha":"Atualmente minha ocupação profissonal é o exército brasileiro onde trabalho com manutenção de computadores"},
+  {"linha":"Sou natural de Campinas-SP, tenho 19 anos"},
+  {"linha":"Minha ultima ocupação profissonal foi em 2017 no exército brasileiro onde trabalhei com manutenção de computadores"},
   {"linha":"Estou cursando o Técnico de Informatica para Internet no Cotuca"},
   {"linha":"ls -l formacao_academica"},
   {"codigo":"<ul><li>Ensino Médio: Colégio Politécnico Bento Quirino </li><li>Técnico em Informatica: Colégio Politécnico Bento Quirino</li>"},
   {"linha":"Apesar de pouca experiência profissional tenho estudado muitas linguagens/tecnologias"},
   {"linha":"ls -l habilidades"},
-  {"codigo":"<ul><li>Java</li><li>JavaScript</li><li>PHP</li><li>MySql</li><li>Microsoft SQL</li><li>Redes</li><li>Linux</li><li>Joomla</li></ul>"},
-  {"linha":"Tenho como objetivo estagiar como programador se possivel trabalhando com back-end"},
+  {"codigo":"<ul><li>Java</li><li>JavaScript</li><li>PHP</li><li>MySql</li><li>Microsoft SQL</li><li>Redes</li><li>Linux</li></ul>"},
+  {"linha":"Tenho como objetivo estagiar como programador se possivel trabalhando com Desenvolvimento Web"},
   {"linha":"ls -l contato"},
   {"codigo":"<ul><li>Gmail: fernandodsds36@gmail.com</li><li>Whatsapp: (19)98290-1075</li></ul>"},
 
